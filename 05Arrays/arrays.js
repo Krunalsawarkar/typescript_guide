f55df497edd14bf95238eec12551cfd05b70b6ec
Krunal Sawarkar <sawarkarkrunal114@gmail.com>
2024-03-24 20:02:49 +0530

    learning Commit

---
+++ 05Arrays/arrays.js
@@ -0,0 +1,8 @@
+var superHeros = [];
+var superPower = [];
+var health = [];
+var allUsers = [];
+superPower.push(23);
+health.push(456);
+superHeros.push("spiderman");
+allUsers.push({ name: "", isActive: true });
---
+++ 05Arrays/arrays.ts
@@ -0,0 +1,19 @@
+const superHeros: string[] = [];
+
+const superPower: number[] = [];
+const health: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+superPower.push(23);
+health.push(456);
+superHeros.push("spiderman");
+
+allUsers.push({ name: "", isActive: true });
+
+export {};
